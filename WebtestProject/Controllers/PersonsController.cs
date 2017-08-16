3e42583e0c1197816e2d74e530de940acf054e1a
71500030 <71500030@OMEGAB06>
2017-08-16 20:04:53 -0300

    Adicionar arquivos de projeto.

---
+++ WebtestProject/Controllers/PersonsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebtestProject.Controllers
+{
+     [RoutePrefix("v1/persons")]
+    public class PersonsController : ApiController
+    {
+
+
+        private string Name { get; set; }
+        private static IList<Person> _persons = new List<Person>();
+
+
+        [Route("{id}/name")]
+        [HttpGet]
+        public string GetNamer(int id)
+        {
+            Name = "tiago";
+            return Name;
+
+        }
+
+        [Route("{id}/old")]
+        [HttpGet]
+        public int Getold(int id)
+        {
+           
+            return id;
+
+        }
+        [Route("{id}/surname")]
+        [HttpGet]
+        public string Getsurname(int id)
+        {
+            return "costa";
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public object Getpersons(int id)
+        {
+            return _persons[id];
+        }
+
+        [Route]
+        [HttpGet]
+        public IList<Person> Getpersons()
+        {
+
+            return _persons;
+
+        }
+
+        [Route]
+        [HttpPost]
+        public Person PostPersons(Person person)
+        {
+            _persons.Add(person);
+            person.id = _persons.Count;
+            return person;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ WebtestProject/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebtestProject
+{
+    public class Person
+    {
+
+        public int id;
+        public string name;
+        public int old;
+        
+    }
+}
